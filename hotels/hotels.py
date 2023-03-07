b72ef3f20fd4d544ab047517b7539acdc79b178f
alexechoi <uceiac8@ucl.ac.uk>
2023-03-07 15:24:29 +0000

    Add hotels webscraping

--- hotels/hotels.py
+++ hotels/hotels.py
@@ -1,113 +1,44 @@
+import json
+import requests
+from bs4 import BeautifulSoup
 
-import asyncio
-from urllib.parse import urlencode
-from httpx import AsyncClient
+url = 'https://www.booking.com/searchresults.en-gb.html?ss=London%2C+Greater+London%2C+United+Kingdom'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
 
-
-async def search_page(
-    query,
-    session: AsyncClient,
-    checkin: str = "",
-    checkout: str = "",
-    number_of_rooms=1,
-    offset: int = 0,
-):
-    """scrapes a single hotel search page of booking.com"""
-    checkin_year, checking_month, checking_day = checkin.split("-") if checkin else "", "", ""
-    checkout_year, checkout_month, checkout_day = checkout.split("-") if checkout else "", "", ""
-
-    url = "https://www.booking.com/searchresults.html"
-    url += "?" + urlencode(
-        {
-            "ss": query,
-            "checkin_year": checkin_year,
-            "checkin_month": checking_month,
-            "checkin_monthday": checking_day,
-            "checkout_year": checkout_year,
-            "checkout_month": checkout_month,
-            "checkout_monthday": checkout_day,
-            "no_rooms": number_of_rooms,
-            "offset": offset,
-        }
-    )
-    return await session.get(url, follow_redirects=True)
-
-
-async def main():
-    """Runs the example code"""
-    # first we need to imitate web browser headers to not get blocked instantly
-    HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8",
-        "Connection": "keep-alive",
-        "Accept-Language": "en-US,en;q=0.9,lt;q=0.8,et;q=0.7,de;q=0.6",
+for page in range(1, 4): # Loop through the first three pages
+    params = {
+        'page': page,
     }
-
-    async with AsyncClient(headers=HEADERS) as session:
-        await search_page("London", session)
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
-    loop.run_forever()
+    response = requests.get(url, headers=headers, params=params)
+    soup = BeautifulSoup(response.text, 'html.parser')
     
-def parse_search_total_results(html: str):
-    """parse total number of results from search page HTML"""
-    sel = Selector(text=html)
-    # parse total amount of pages from heading1 text:
-    # e.g. "London: 1,232 properties found"
-    total_results = int(sel.css("h1").re("([\d,]+) properties found")[0].replace(",", ""))
-    return total_results
+    property_blocks = soup.find_all('div', {'data-testid': 'property-card'})
+    for block in property_blocks:
+        # Extract the hotel name
+        hotel_name_div = block.find('a', {'data-testid': 'title-link'})
+        if hotel_name_div is not None:
+            hotel_name = hotel_name_div.text.strip().replace('Opens in new window', '')
+            print(hotel_name)
 
+        # Extract the hotel location
+        hotel_location_div = block.find('div', {'data-testid': 'location'})
+        if hotel_location_div is not None:
+            hotel_location = hotel_location_div.find('span', {'class': 'f4bd0794db'}).text.strip()
+            print(hotel_location)
 
-def parse_search_page(html: str):
-    """parse hotel preview data from search page HTML"""
-    sel = Selector(text=html)
+        # Extract the number of reviews
+        review_div = block.find('div', {'class': 'f919b8b3d5'})
+        if review_div is not None:
+            review_count = review_div.find('span', {'class': 'cb5ebe3ffb'}).text.strip()
+            print(review_count)
 
-    hotel_previews = {}
-    for hotel_box in sel.xpath('//div[@data-testid="property-card"]'):
-        url = hotel_box.xpath('.//h3/a[@data-testid="title-link"]/@href').get("").split("?")[0]
-        hotel_previews[url] = {
-            "name": hotel_box.xpath('.//h3/a[@data-testid="title-link"]/div/text()').get(""),
-            "location": hotel_box.xpath('.//span[@data-testid="address"]/text()').get(""),
-            "score": hotel_box.xpath('.//div[@data-testid="review-score"]/div/text()').get(""),
-            "review_count": hotel_box.xpath('.//div[@data-testid="review-score"]/div[2]/div[2]/text()').get(""),
-            "stars": len(hotel_box.xpath('.//div[@data-testid="rating-stars"]/span').getall()),
-            "image": hotel_box.xpath('.//img[@data-testid="image"]/@src').get(),
-        }
-    return hotel_previews
+        # Add hotel name and location to hotels list
+        hotels.append({'name': hotel_name, 'location': hotel_location})
+        
+# Convert hotels list to JSON format
+json_data = json.dumps(hotels, indent=4)
 
-
-async def scrape_search(
-    query,
-    session: AsyncClient,
-    checkin: str = "",
-    checkout: str = "",
-    number_of_rooms=1,
-):
-    """scrape all hotel previews from a given search query"""
-    first_page = await search_page(
-        query=query, session=session, checkin=checkin, checkout=checkout, number_of_rooms=number_of_rooms
-    )
-    print('Scraping first page')
-    total_results = parse_search_total_results(first_page.text)
-    print(f'Total results:{total_results}')
-    other_pages = await asyncio.gather(
-        *[
-            search_page(
-                query=query,
-                session=session,
-                checkin=checkin,
-                checkout=checkout,
-                number_of_rooms=number_of_rooms,
-                offset=offset,
-            )
-            for offset in range(25, total_results, 25)
-        ]
-    )
-    hotel_previews = {}
-    for response in [first_page, *other_pages]:
-        hotel_previews.update(parse_search_page(response.text))
-    return hotel_previews
+# Print JSON data
+print(json_data)
