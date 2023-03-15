66b71324a5a43e7438316ff1d848418d1f90d0cb
zeynepmertan <zeynep.mertan.19@ucl.ac.uk>
2023-03-15 13:18:40 +0000

    Update geolocation_hotels.py
    Update script to run on EC2

---
+++ geolocation/geolocation_hotels.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+def get_coordinates_from_file(json_file_path, api_key):
+    """
+    Retrieves latitude and longitude coordinates for each location in a JSON file, and writes the results to a new JSON file.
+    """
+    def get_coordinates(location, api_key):
+        """
+        Returns the latitude and longitude coordinates of a location based on its name.
+        """
+        url = 'https://maps.googleapis.com/maps/api/geocode/json'
+        params = {'address': location, 'key': api_key}
+        response = requests.get(url, params=params)
+        response_data = response.json()
+        if response_data['status'] == 'OK':
+            latitude = response_data['results'][0]['geometry']['location']['lat']
+            longitude = response_data['results'][0]['geometry']['location']['lng']
+            return latitude, longitude
+        else:
+            print('Could not find coordinates for', location)
+            return None
+
+    with open(json_file_path, 'r') as f:
+        data = json.load(f)
+
+    coordinates = {}
+    for hotel in data:
+        location = hotel['location']
+        coords = get_coordinates(location, api_key)
+        if coords is not None:
+            coordinates[hotel['name']] = {'latitude': coords[0], 'longitude': coords[1]}
+
+    with open('/home/ubuntu/output/coordinates_hotel.json', 'w') as f:
+        json.dump(coordinates, f)
+
+    print('Coordinates saved to coordinates.json')
+
+
+api_key='***REMOVED***'
+
+hotels = '/home/ubuntu/output/hotels.json'
+
+get_coordinates_from_file(hotels, api_key)
