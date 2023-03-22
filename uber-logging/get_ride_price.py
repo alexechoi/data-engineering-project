86a26e232fae77b80c26e94a26c07292b387da95
alexechoi <uceiac8@ucl.ac.uk>
2023-03-22 13:36:46 +0000

    Update get_ride_price()
    take a JSON file as input instead so many routes can be queried appropriately

--- uber-logging/get_ride_price.py
+++ uber-logging/get_ride_price.py
@@ -3,8 +3,17 @@ import os
 import requests
 import time
 
-def get_ride_price(origin_latitude, origin_longitude, destination_latitude, destination_longitude):
+def get_ride_price(json_file_path):
     url = "https://www.uber.com/api/loadFEEstimates?localeCode=en"
+    
+    with open(json_file_path, 'r') as f:
+        data = json.load(f)
+        for route_data in data.values():
+            origin_latitude = route_data['start_coordinate']['latitude']
+            origin_longitude = route_data['start_coordinate']['longitude']
+            destination_latitude = route_data['end_coordinate']['latitude']
+            destination_longitude = route_data['end_coordinate']['longitude']
+
 
     payload = json.dumps({
         "origin": {
@@ -26,22 +35,35 @@ def get_ride_price(origin_latitude, origin_longitude, destination_latitude, dest
     response = requests.request("POST", url, headers=headers, data=payload)
     result = [[x['vehicleViewDisplayName'], x['fareString']] for x in response.json()['data']['prices']]
 
-    # Add timestamp and readable timestamp to result
+    # Add timestamp, latitude, and longitude to result
     timestamp = int(time.time())
     readable_timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
-    result_with_timestamp = {"timestamp": timestamp, "readable_timestamp": readable_timestamp, "prices": result}
+    result_with_timestamp = {
+        "timestamp": timestamp, 
+        "readable_timestamp": readable_timestamp, 
+        "origin_latitude": origin_latitude,
+        "origin_longitude": origin_longitude,
+        "destination_latitude": destination_latitude,
+        "destination_longitude": destination_longitude,
+        "prices": result
+    } 
 
+    # Print current working directory
+    print("Current working directory:", os.getcwd())
+    
     # Append result with timestamp to file
-    output_dir = "../output/"
+    output_dir = "/home/ubuntu/output/"
     os.makedirs(output_dir, exist_ok=True)
-    file_path = os.path.join(output_dir, "ride_prices.json")
-    if os.path.exists(file_path):
+    file_path = os.path.join(output_dir, "uber.json")
+    print("Output file path:", os.path.abspath(file_path))
+    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
         with open(file_path, "r") as f:
             data = json.load(f)
     else:
         data = []
     data.append(result_with_timestamp)
-    with open(file_path, "w") as f:
+    with open(file_path, "w+") as f:
         json.dump(data, f)
-
+    print('Ride prices saved to uber.json')
+    
     return result_with_timestamp
