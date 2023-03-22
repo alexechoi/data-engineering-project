936beb8e3ef8bec7ac467a629e8f275c45d4902f
alexechoi <uceiac8@ucl.ac.uk>
2023-03-22 13:45:03 +0000

    Update s3-upload shell script capabilities
    Shell script capabilities to execute every hour and add an archive folder if it is not already made. Remove files from output folder to archive folder.

--- pipeline/s3-upload.sh
+++ pipeline/s3-upload.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
 # Set the S3 bucket name
-S3_BUCKET_NAME="alex-choi-ucl-test-1"
+S3_BUCKET_NAME="***REMOVED***"
 
-# Loop through all .json files in the current directory
-for file in *.json
+# Create the archive directory if it does not exist
+if [ ! -d "archive" ]
+then
+  mkdir archive
+  echo "Archive directory created."
+fi
+
+# Loop every hour
+while true
 do
-  # Copy the file to the S3 bucket using the AWS CLI
-  aws s3 cp "$file" "s3://$S3_BUCKET_NAME/"
+  # Loop through all .json files in the output directory
+  for file in output/*.json
+  do
+    # Copy the file to the S3 bucket using the AWS CLI
+    aws s3 cp "$file" "s3://$S3_BUCKET_NAME/"
+
+    # Move the file to the archive folder
+    mv "$file" archive/
+    echo "Moved $file to archive/"
+  done
+  
+  echo "All files moved to S3 and archive."
+  
+  # Sleep for an hour before looping again
+  sleep 3600
 done
